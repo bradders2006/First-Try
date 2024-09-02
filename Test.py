a5c3013ffa36a7d75512175e4132843e945782b6
Bradley <bradleyr06@Bradleys-MBP.broadband>
2024-09-02 22:11:26 +0100

    Small changes

--- Test.py
+++ Test.py
@@ -18,22 +18,23 @@ class User:
             connection.commit()
 
             # Clear window and display signup success message
-            for widgets in signup_form.winfo_children():
+            for widgets in gameWindow.winfo_children():
                 widgets.destroy()
-                Label(signup_form, text="Sign Up Successful").grid(row=0)
+                Label(gameWindow, text="Sign Up Successful").grid(row=0)
+                gameWindow.title("Main Game")
 
         except sqlite3.IntegrityError as e: # error handling if user tries to signup with an existing username
             # Clear any existing messages
-            for widget in signup_form.grid_slaves(row=3, column=0):
+            for widget in gameWindow.grid_slaves(row=3, column=0):
                 widget.destroy()
-            Label(signup_form, text="Error: Username already exists.", fg="red").grid(row=3)
+            Label(gameWindow, text="Error: Username already exists.", fg="red").grid(row=3)
 
         # Close the connection
         connection.close()
 
-def create(username, password): # initialise User object
-    user_details = User(username, password)
-    user_details.add_user()
+#def create(username, password): # initialise User object
+ #   user_details = User(username, password)
+  #  user_details.add_user()
 
 def on_click():
     # Retrieve the values from the entry fields and call create
@@ -42,13 +43,14 @@ def on_click():
     password_result = password_check(username, password)
 
     # Clear any existing messages
-    for widget in signup_form.grid_slaves(row=3, column=0):
+    for widget in gameWindow.grid_slaves(row=3, column=0):
         widget.destroy()
         
     if password_result == True:
-        create(username, password) # take user to 
+        user_details = User(username, password)
+        user_details.add_user()
     else:
-        Label(signup_form, text=password_result, fg="red").grid(row=3)
+        Label(gameWindow, text=password_result, fg="red").grid(row=3)
         
 
 # Function to validate the password
@@ -75,27 +77,32 @@ def password_check(username, passwd):
     return val
     
 
-signup_form = Tk() # create main window
+gameWindow = Tk() # create main window
 
 # Getting screen width and height of display
-width= signup_form.winfo_screenwidth() 
-height= signup_form.winfo_screenheight()
+width= gameWindow.winfo_screenwidth() 
+height= gameWindow.winfo_screenheight()
 
 # Setting tkinter window size
-signup_form.geometry("%dx%d" % (width, height))
-signup_form.title("Signup Form")
+gameWindow.geometry("%dx%d" % (width, height))
+
+
+
+
+# Signup form window widgets
+gameWindow.title("Signup Form")
 
 # Create Username and Password input boxes
-Label(signup_form, text="Username").grid(row=0)
-Label(signup_form, text="Password").grid(row=1)
+Label(gameWindow, text="Username").grid(row=0)
+Label(gameWindow, text="Password").grid(row=1)
 
-e1 = Entry(signup_form)
-e2 = Entry(signup_form, show="*")
+e1 = Entry(gameWindow)
+e2 = Entry(gameWindow, show="*")
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
 
 # Create button
-button = Button(signup_form, text="Sign Up", command=on_click)
+button = Button(gameWindow, text="Sign Up", command=on_click)
 button.grid(row=2, column=0, columnspan=2) # position button
 
 mainloop()
